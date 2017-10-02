b81e1e20d01584fd69d58b75138188e2054d4e42
dariusPan <pjm.darius@gmail.com>
2017-10-02 19:35:24 +0800

    First

---
+++ tft.sdk/tft/Debug/src/lab3_vga.d
@@ -0,0 +1,43 @@
+src/lab3_vga.o src/lab3_vga.o: ../src/lab3_vga.c \
+ ../../tft_bsp/ps7_cortexa9_0/include/xtft.h \
+ ../../tft_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../tft_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../tft_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../tft_bsp/ps7_cortexa9_0/include/xtft_hw.h \
+ ../../tft_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../tft_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../tft_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../tft_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../tft_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../tft_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../tft_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../tft_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../tft_bsp/ps7_cortexa9_0/include/xuartps_hw.h
+
+../../tft_bsp/ps7_cortexa9_0/include/xtft.h:
+
+../../tft_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../tft_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../tft_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../tft_bsp/ps7_cortexa9_0/include/xtft_hw.h:
+
+../../tft_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../tft_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../tft_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../tft_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../tft_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../tft_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../tft_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../tft_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../tft_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
